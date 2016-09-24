409ee876a899c1d6d9e41e163d34bf8fddedd493
enxo <asadidrees89@gmail.com>
2016-09-24 16:11:16 +0500

    3rd Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,98 @@
+var clickInfo=0;
+var clickInfo2=0;
+
+
+function hideshow1()
+{
+	var show01=document.getElementsByClassName("clickable")[0].style;
+	var show1=document.getElementById("ass-41").style;
+	if(clickInfo===0)
+	{
+		show01.padding="1em";
+		show01.margin="0";
+		show01.textalign="left"
+		show01.backgroundcolor="white";
+		show1.display="block";
+		clickInfo=1;
+	}
+	else
+	{
+		show01.padding="";
+		show01.margin="";
+		show01.textalign=""
+		show01.backgroundcolor="";
+		show1.display="none";
+		clickInfo=0;	
+	}
+}
+
+
+function hideshow2()
+{
+	var show02=document.getElementsByClassName("clickable")[1].style;
+	var show2=document.getElementById("ass-42").style;
+	if(clickInfo2===0)
+	{
+		show02.padding="1em";
+		show02.margin="0";
+		show02.textalign="left"
+		show02.backgroundcolor="white";
+		show2.display="block";
+		clickInfo2=1;
+	}
+	else
+	{
+		show02.padding="";
+		show02.margin="";
+		show02.textalign=""
+		show02.backgroundcolor="";
+		show2.display="none";
+		clickInfo2=0;	
+	}
+}
+
+function clearCell(x)
+{
+	if(x===1)
+	{
+	var clearNameCell=document.getElementById("user-id");
+	clearNameCell.value="";
+	}
+	else
+	{
+	var clearNameCell=document.getElementById("input-limit");
+	clearNameCell.value="";
+	}
+}
+
+function clearCell2(x)
+{
+	if(x===1)
+	{
+	var clearNameCell=document.getElementById("user-id2");
+	clearNameCell.value="";
+	}
+	else
+	{
+	var clearNameCell=document.getElementById("limit-2");
+	clearNameCell.value="";
+	}
+}
+
+								/* FOR ASSIGNMENT 1*/
+
 var sumA=0;
 var sumB=0;
 var sumTot=0;
 var occA=0;
 var occB=0;
 
-function clearCell()
-{
-	var clearNameCell=document.getElementById("user-id");
-	clearNameCell.value="";
-}
-
-function clearCell2()
-{
-	var clearNameCell=document.getElementById("input-limit");
-	clearNameCell.value="";
-}
-
-function getValues()
+function getValues() 		
 {
 var name=document.getElementById('user-id').value;
 var int1=document.getElementById('intj-1').value;
 var int2=document.getElementById('intj-2').value;
 var lim=document.getElementById('input-limit').value;
-console.log(name);
-console.log(int1);
-console.log(int2);
-console.log(lim);
 
 
 for(count=1;count<lim;count++)
@@ -50,12 +117,6 @@ for(count=1;count<lim;count++)
 	}
 	
 }
-console.log("Well, "+name+", the results are as")
-console.log("Multiples of "+int1+" had "+occA+" occurences");
-console.log("Sum of all the multiples of "+int1+" is "+sumA);
-console.log("Multiples of "+int2+" had "+occB+" occurences");
-console.log("Sum of all the multiples of "+int2+" is "+sumB);
-console.log("Sum of all the multiples of "+int1+" and "+int2+" is "+sumTot);
 
 var textChange = document.getElementById("username");
 textChange.textContent =name;
@@ -82,5 +143,81 @@ sumTot=0;
 occA=0;
 occB=0;
 
+
+}
+										/*FOR ASSIGNMENT 2*/
+
+var fibAdd=0;
+var fibPrev1=0;
+var fibSeq=1;
+var fibSum=0;
+var count=0;
+var typeCount=0;
+
+function getValues2() 					
+{
+	var name=document.getElementById('user-id2').value;
+	var lim=document.getElementById('limit-2').value;
+	var sumType=document.getElementById('sumType').value;
+
+
+		while(fibSeq+fibAdd<=lim)
+		{
+			fibPrev1=fibSeq;
+			fibSeq+=fibAdd;
+			fibAdd=fibPrev1;
+			count+=1;
+
+			switch(sumType)
+			{
+				case("Even"):
+					if((fibSeq%2)===0)
+					{	
+						fibSum+=fibSeq;
+						typeCount+=1;
+					}
+					break;
+				case("Odd"):
+					if((fibSeq%2)===1)
+					{	
+						fibSum+=fibSeq;
+						typeCount+=1;
+					}
+					break;
+				case("All"):
+						fibSum+=fibSeq;
+						typeCount+=1;
+					break;
+				default:			
+						break;
+			};
+		}
+
+
+var textChange = document.getElementById("username2");
+textChange.textContent =name;
+var textChange = document.getElementById("occ1b");
+textChange.textContent =count;
+var textChange = document.getElementsByClassName("typeQuan")[0];
+textChange.textContent =typeCount;
+var textChange = document.getElementsByClassName("typeQuan")[1];
+textChange.textContent =typeCount;
+var textChange = document.getElementById("type");
+textChange.textContent =sumType;
+var textChange = document.getElementById("highTerm");
+textChange.textContent =fibSeq;
+var textChange = document.getElementById("total2");
+textChange.textContent =fibSum;
+
+var result=document.getElementById("answer-announcer2").style;
+result.visibility="visible";
+
+
+fibAdd=0;
+fibPrev1=0;
+fibSeq=1;
+fibSum=0;
+count=0;
+typeCount=0;
 
 }
